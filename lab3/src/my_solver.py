aa99f6fd0254098f684dc230be6aa3918db11e74
kucjan <janekkuc00@gmail.com>
2022-11-22 01:08:31 +0100

    add module, initialize solver

---
+++ lab3/src/my_solver.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.path.append("/Users/janekkuc/Desktop/PW/Sem7/WSI/wsi22Z-kuc/lab3/two-player-games")
+
+from two_player_games.games.Pick import *
+
+def minimax(state, depth, alpha, beta, player_char):
+  if player_char == 1:
+    pass
+
+if __name__ == '__main__':
+  
+  p = Pick()
+  
+  print(p.first_player.char)
